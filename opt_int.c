5ee06059ec236fcfb202eefecf93d0301fe42cc2
xica369 <761@holbertonschool.com>
2019-03-19 22:55:59 +0000

    function i and d, now prints 0 too

--- opt_int.c
+++ opt_int.c
@@ -7,46 +7,46 @@
  *
  *Return: the number of bytes
  */
-int op_int(va_list l)
+int op_int(int a)
 {
-	int a = 0, b = 0, c = 0, d = 0, e = 1, f = 0, m = 0, n = 0;
+	int b, c = 0, d = 0, e = 1, f = 0, m = 0, n = 0;
 	char x;
 
-	a = va_arg(l, int);
+	/*a = va_arg(l, int);*/
 	f = a;
 	if (a == -2147483648)
 		a = a + 1;
 	if (a < 0)
-	{
-		_putchar('-');
+	{	_putchar('-');
 		a = a * -1;
 	}
 	d = a;
 	for (b = 0; a != 0; b++)
-	{
-		a = a / 10;
+	{	a = a / 10;
 		if (c == 1)
 			e = e * 10;
 		c = 1;
 	}
 	c = b;
 	for (; b > 0; b--)
-	{
-		m = d / e;
+	{	m = d / e;
 		n = m % 10;
 		e = e / 10;
 		if (m < 10)
-		{
-			x = m + '0';
+		{	x = m + '0';
 			_putchar(x);
 		}
 		else
-		{
-			x = n + '0';
+		{	x = n + '0';
 			if (f == -2147483648 && e == 0)
 				x = '8';
 			_putchar(x);
 		}
+	}
+	if (a == 0)
+	{	x = '0';
+		_putchar(x);
+		c = 1;
 	}
 	return (c);
 }
